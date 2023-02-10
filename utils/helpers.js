0fa6b1d29fe0e5953be4540c49487c15779c9cca
Laura Trapaga <trapaga9@gmail.com>
2023-02-09 20:31:34 -0500

    Add helpers.js code

---
+++ utils/helpers.js
@@ -0,0 +1,22 @@
+module.exports = {
+  capitalize_first_name: (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+  capatilize_last_name: (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+  format_time: (time) => {
+    return new Date(time).toLocaleString("en-US", {
+      hour12: true,
+      hourCycle: "h12",
+      hour: "numeric",
+      minute: "2-digit",
+    });
+  },
+  get_chat_class: (senderId, currentId) => {
+    if (senderId !== currentId) {
+      return "received";
+    }
+    return "sent";
+  },
+};
