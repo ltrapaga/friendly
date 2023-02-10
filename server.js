5560d7fa1ed8ed243bd1a9386afeede04d60b960
Laura Trapaga <trapaga9@gmail.com>
2023-02-09 21:10:12 -0500

    Format all current files

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const session = require("express-session");
-const express = require("express");
-const path = require("path");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const exphbs = require("express-handlebars");
-const helpers = require("./utils/helpers");
+const session = require('express-session');
+const express = require('express');
+const path = require('path');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: process.env.DB_SECRET,
@@ -29,16 +29,16 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-io.on("connection", (socket) => {
-  socket.on("new message", (message) => {
-    io.emit("new message", message);
+io.on('connection', (socket) => {
+  socket.on('new message', (message) => {
+    io.emit('new message', message);
   });
 });
 
