dcbebb3fb4d0a0d11c9563f7e577d9dfe16e092c
Calvin Chern <calvin.chern24@gmail.com>
2023-02-10 21:10:57 -0500

    comments for delete-user js

--- public/js/delete-user.js
+++ public/js/delete-user.js
@@ -1,10 +1,13 @@
 async function deleteTheUser() {
+    // creating a logout button for the User
     const logoutButton = document.getElementById('logout-btn');
 
+    // creating a button to generate randomization of a new friend
     const liveUserId = parseInt(
         document.querySelector('#random-btn').getAttribute('user-data')
     );
 
+    // fetching api to delete user
     const userResponse = await fetch('/api/users/' + liveUserId, {
         method: 'delete',
         headers: { 'Content-Type': 'application/json' }
@@ -17,5 +20,5 @@ async function deleteTheUser() {
     }
 
 }
-
+// creating a delete button for the User
 document.querySelector('#delete-btn').addEventListener('click', deleteTheUser)
