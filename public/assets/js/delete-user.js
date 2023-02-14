d3cc64526fea0e78972475a20ea77033f026e4d9
Calvin Chern <calvin.chern24@gmail.com>
2023-02-13 19:22:00 -0500

    Update js files and handlebars

---
+++ public/assets/js/delete-user.js
@@ -0,0 +1,21 @@
+async function deleteUserForm() {
+  const logoutButton = document.getElementById('logout-btn');
+
+  const thisUserId = parseInt(
+    document.querySelector('#random-btn').getAttribute('user-Data')
+  );
+
+  const response = await fetch('/api/users/' + thisUserId, {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  // logging out user when deleted
+  if (response.ok) {
+    logoutButton.click(); 
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#delete-btn').addEventListener('click', deleteUserForm);
